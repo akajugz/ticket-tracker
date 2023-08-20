89a0f393e71e9eadbede5c6b355bf73ea692f4ce
Jugraj Singh <jsbains96@gmail.com>
2023-08-21 00:59:27 +0100

    The profiles page works and displays the information for the employees without the ticket counter

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,11 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 import { team } from "./data/data";
 import NavMenu from "./components/NavMenu/NavMenu";
 import HomePage from "./Containers/HomePage/HomePage";
-import Profiles from "./components/Profile/Profile";
 import TicketsPage from "./Containers/TrackerPage/TrackerPage";
+import EmployeesPage from "./Containers/EmployeesPage/EmployeesPage";
 import "./App.scss";
 
-type ProfileProps = {
-  id: number;
-  name: string;
-  role: string;
-  profile: {
-    experience: string;
-    department: string;
-    techstack: string[];
-    profilePicture: string;
-  };
-};
-
-const App = ({ name, role }: ProfileProps) => {
+const App = () => {
   return (
     <>
       <HashRouter>
@@ -26,22 +14,7 @@ const App = ({ name, role }: ProfileProps) => {
           <NavMenu />
           <Routes>
             <Route path="/" element={<HomePage employee={team} />} />
-            <Route
-              path="/profiles"
-              element={
-                <Profiles
-                  id={0}
-                  name={name}
-                  role={role}
-                  profile={{
-                    experience: "",
-                    department: "{profile.department}",
-                    techstack: [],
-                    profilePicture: "",
-                  }}
-                />
-              }
-            />
+            <Route path="/profiles" element={<EmployeesPage team={team} />} />
             <Route path="/employee" element={<TicketsPage />} />
           </Routes>
         </div>
--- src/Containers/EmployeesPage/EmployeesPage.tsx
+++ src/Containers/EmployeesPage/EmployeesPage.tsx
@@ -1,11 +1,30 @@
-import Employee from "../../components/Employee/Employee";
-import { team } from "../../data/data";
+import Profiles from "../../components/Profile/Profile";
 
-const EmployeesPage = () => {
+type EmployeesPageProps = {
+  team: {
+    id: number;
+    name: string;
+    role: string;
+    profile: {
+      experience: string;
+      department: string;
+      techstack: string[];
+      profilePicture: string;
+    };
+  }[];
+};
+
+const EmployeesPage: React.FC<EmployeesPageProps> = ({ team }) => {
   return (
     <div>
       {team.map((employee) => (
-        <Employee key={employee.id} {...employee} />
+        <Profiles
+          key={employee.id}
+          name={employee.name}
+          role={employee.role}
+          profile={employee.profile}
+          id={0}
+        />
       ))}
     </div>
   );
